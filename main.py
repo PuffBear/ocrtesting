a9147459e00c4366d0308563487ad44bbd660789
Agriya Yadav <agriyayadav@gmail.com>
2025-06-05 11:56:10 +0530

    Commented out testing script from main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from utils.file_utils import convert_pdf_to_images
+# file_utils.py testing script:
+"""from utils.file_utils import convert_pdf_to_images
 
 image_list = convert_pdf_to_images("fir2017.pdf")
 print(image_list)
+"""
